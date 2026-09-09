a2ff4b98985ab013e2efa97600b78d6bf48d44bf
Esteban <estebanlucaspark@gmail.com>
2026-09-09 19:59:36 -0300

    listas y matrices

---
+++ TrabajoIntegrador/datos.py
@@ -0,0 +1,29 @@
+# Lista de contenidos:
+contenidos = [“Game Of Thrones”, “Shrek”, “Cars”]
+
+# Lista de Usuarios: 
+usuarios = [“Esteban”, “Sofi”, “Lucca”,”Mariangel”, “Pepe”, “Roberto”, “Andres”, “Luciana”, “Micaela”, “Tomas”]
+
+visualizaciones = [
+    [3, 1, 4],  # Esteban
+    [2, 4, 1],  # Sofi
+    [0, 2, 3],  # Lucca
+    [1, 5, 2],  # Mariangel
+    [4, 0, 1],  # Pepe
+    [2, 3, 5],  # Roberto
+    [5, 1, 0],  # Andres
+    [1, 4, 3],  # Luciana
+    [3, 2, 4],  # Micaela
+    [0, 3, 2]   # Tomas
+]
+
+
+# Lista de géneros:
+generos = [“Fantasía”, “Ciencia Ficción”, “Acción”]
+
+# Tupla de tipos:
+tipos = (“Película”, “Serie”, “Show”)
+
+# Lista de códigos de contenidos:
+codigosContenidos = [“C01”, “C02”, “C03”]
+
